573a757f7ec43d77671573630716f8f226338ce6
mayan_sharma <41385368+mayan-sharma@users.noreply.github.com>
2019-10-13 04:11:51 +0530

    Add files via upload

---
+++ PeePeePooPoo/main.js
@@ -0,0 +1,115 @@
+let video;
+let poseNet;
+let poses = [];
+/*getPose= () => {
+
+	poseNet.on("pose", function(results) {
+	poses = results;
+	
+	// checkPose(poses);
+	// if (flag) {
+	//   console.log(poses);
+	//   flag = false;
+	// }
+  }
+	// return poses;
+  
+} */
+findSlope=(x1,y1,x2,y2)=>{
+	let slope = (x1-x2)/ (y1-y2)
+	return slope;
+}
+
+checkColinear = (x1,y1,x2,y2, x3, y3) => {
+	let slope1 = findSlope(x1,y1,x2,y2);
+	let slope2 = findSlope(x2,y2,x3,y3);
+	return slope1==slope2;
+}
+
+checkPose = keypointsArr => {
+  //let x15, y15, x13, y13, x11, y11;
+  let x[17], y[17];
+  // x15 = keypointsArr[15].position.x;
+  for(var i=0;i<17;i++)
+  {
+	x[i] = keypointArr[i].position,x;
+  }
+  for(var i=0;i<17;i++)
+  {
+	y[i] = keypointArr[i].position,x;
+  }
+  
+
+};
+
+function setup() {
+  let cnv = createCanvas(640, 480);
+  cnv.style("border-radius", "2rem");
+  video = createCapture(VIDEO);
+  video.size(width, height);
+  let flag = true;
+  // Create a new poseNet method with a single detection
+  poseNet = ml5.poseNet(video, modelReady);
+  // This sets up an event that fills the global variable "poses"
+  // with an array every time new poses are detected
+  poseNet.on("pose", function(results) {
+	poses = results;
+	if (flag) {
+	  console.log(poses);
+	  flag = false;
+	}
+  });
+  // Hide the video element, and just show the canvas
+  video.hide();
+}
+
+function modelReady() {
+  select("#status").html("Model Loaded");
+}
+
+function draw() {
+  image(video, 0, 0, width, height);
+
+  // We can call both functions to draw all keypoints and the skeletons
+  drawKeypoints();
+  drawSkeleton();
+}
+
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints() {
+  // Loop through all the poses detected
+  for (let i = 0; i < poses.length; i++) {
+	// For each pose detected, loop through all the keypoints
+	let pose = poses[i].pose;
+	for (let j = 0; j < pose.keypoints.length; j++) {
+	  // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+	  let keypoint = pose.keypoints[j];
+	  // Only draw an ellipse is the pose probability is bigger than 0.2
+	  if (keypoint.score > 0.2) {
+		fill(255, 0, 0);
+		noStroke();
+		ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+	  }
+	}
+  }
+}
+
+// A function to draw the skeletons
+function drawSkeleton() {
+  // Loop through all the skeletons detected
+  for (let i = 0; i < poses.length; i++) {
+	let skeleton = poses[i].skeleton;
+	// For every skeleton, loop through all body connections
+	for (let j = 0; j < skeleton.length; j++) {
+	  let partA = skeleton[j][0];
+	  let partB = skeleton[j][1];
+	  stroke(255, 0, 0);
+	  line(
+		partA.position.x,
+		partA.position.y,
+		partB.position.x,
+		partB.position.y
+	  );
+	}
+  }
+}
